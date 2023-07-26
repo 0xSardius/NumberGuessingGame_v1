ed331056c19a886b4b554120d0eb790a2368b53a
0xSardius <95114005+0xSardius@users.noreply.github.com>
2023-07-26 20:05:08 +0000

    gm

---
+++ GuessingGame3Guesses.py
@@ -0,0 +1,19 @@
+import random
+
+def guessing_game() :
+  answer = random.randint(1, 100)
+  guess = int(input('Guess a number: '))
+  
+  counter = 2
+  while counter < 2 :
+    
+    if guess == answer :
+      print(f"Good shit! You got it right. The answer is {guess}")
+      break
+    elif guess < answer :
+      print(f"Your guess of {guess} is too low!")
+    else :
+      print(f"Your guess of {guess} is too high!")
+  
+
+guessing_game()
